d5314ac45a32e81bc5e2f8ce65e5ca881e2b8d79
Matthieu42 <matthieu@Matthieu-Fixe.localdomain>
2020-04-14 11:35:25 +0200

    Encore mieux

--- TP2/TP2.c
+++ TP2/TP2.c
@@ -5,39 +5,36 @@
 #include <time.h>
 #include <stdbool.h>
 
-#define SIZE 14
+#define SIZE 10
 #define SIZE_DOMAIN 5
 
-void solve(int somme, int i, int z, int nbval);
+void solve(int somme, int i, int z);
 
-int sol[SIZE];
+int domain[] = {2, 3, 5, 7, 9};
+int nbSol = 0;
 
 int main(int argc, char **argv)
 {
     clock_t start = clock();
-    solve(0, 0, 72, SIZE);
+    solve(0, 0, 52);
     clock_t end = clock();
     double time = ((double)end - start) / CLOCKS_PER_SEC;
-    printf("Time of exec : %f\n", time);
+    printf("Number of solutions : %d\n", nbSol);
+    printf("Time of exec : %f s\n", time);
 }
 
-int domain[] = {2, 3, 5, 7, 9};
 
-void solve(int somme, int i, int z, int nbval)
+void solve(int somme, int i, int z)
 {
-    if (i < nbval && somme < z)
+    if (i < SIZE && somme < z)
     {
         for (int j = 0; j < SIZE_DOMAIN; j++)
         {
-            sol[i] = domain[j];
-            solve(somme + domain[j], i + 1, z, nbval);
+            solve(somme + domain[j], i + 1, z);
         }
     }
-    if (i == nbval && somme == z)
+    if (i == SIZE && somme == z)
     {
-        int k;
-        for (k = 0; k < nbval; k++)
-            printf("%d ", sol[k]);
-        putchar('\n');
+        nbSol++;
     }
 }
